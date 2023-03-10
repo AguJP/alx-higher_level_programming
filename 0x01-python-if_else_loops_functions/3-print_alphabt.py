f2f4967c0010ab3f1bb7de765017e31324f691ed
AguJP <agujpa@gmail.com>
2023-03-10 10:16:59 -0800

    Print alpha !q,!e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@
 """Print lowercase ASCII alphabets excluding q and e"""
 for letter in range(97, 123):
     if chr(letter) is not 'q' and chr(letter) is not 'e':
-           print("{}".format(chr(letter)), end="")
+        print("{}".format(chr(letter)), end="")
