1a1274f899b2c7166a4baafbaf115b7d615456ab
AguJP <agujpa@gmail.com>
2023-03-14 02:17:43 -0700

    Element at

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 
 def element_at(my_list, idx):
     """Function to retrieve an element from a list like in C"""
-    if (idx < 0) || (idx >= len(my_list)):
+    if (idx < 0) || (idx > len(my_list) - 1):
         return (None)
     return (my_list[idx])
