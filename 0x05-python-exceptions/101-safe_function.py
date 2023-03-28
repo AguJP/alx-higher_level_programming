e7fcaaea27e4be71213d208f1b2fe04573380edb
AguJP <agujpa@gmail.com>
2023-03-27 21:00:18 -0700

    Imported sys into files

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import sys
+
 def safe_function(fct, *args):
     """Executes a function safely, returns the result of the function
     if successful, else an exception occurs"""
