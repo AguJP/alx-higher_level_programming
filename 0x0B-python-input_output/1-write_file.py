d1c8b9cf52d94577e91f29a080ebc53fdfa5166d
AguJP <agujpa@gmail.com>
2023-04-12 20:41:33 -0700

    Write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Function to write a string to a text file, returns no. of chars written"""
+def write_file(filename="", text=""):
+    """write to a text file utf-8.
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The no. of characters written
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return (f.write(text))
