c98799c4f3ebf9587c4e839968728a9641b30e4f
AguJP <agujpa@gmail.com>
2023-04-12 20:48:48 -0700

    Append write

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Function to append a string to a textfile"""
+def append_write(filename="", text=""):
+    """Append a string to a text file.
+    Args:
+        filename (str): The name of the file to append.
+        text (str): The string to append to the file.
+    Returns:
+        The no. of characters appended.
+    """
+    with open(filename, "a", encoding="utf-8")as f:
+        return f.append(text)
