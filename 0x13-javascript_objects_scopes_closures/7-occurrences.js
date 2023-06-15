874ff45f34f8b682d26d05501c21c1c53924d5bd
AguJP <agujpa@gmail.com>
2023-06-15 21:53:10 +0000

    Occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let nOccurences = 0;
-  for (let i = 0; i < list.lenght; i++) {
+  for (let i = 0; i < list.length; i++) {
     if (searchElement === list[i]) {
       nOccurences++;
     }
