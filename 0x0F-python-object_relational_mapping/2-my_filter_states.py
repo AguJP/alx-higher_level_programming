9dbab631c4f7db04344263bd1192c1f0cc33ff52
AguJP <agujpa@gmail.com>
2023-08-24 04:45:44 -0700

    My filter states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,7 @@ Usage: ./2-my_filter_states.py <mysql username> \
                                 <state name searched>
 """
 import sys
-import MySQLsb
+import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost",
