2100acf2eccbfce00f70a877b25c2e0a3d8a174e
AguJP <agujpa@gmail.com>
2023-08-24 05:53:56 -0700

    My filter states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Takes in an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument.
+Display all values in the `states` table of the database hbtn_0e_0_usa
+whose name matches that supplied as argument.
 Usage: ./2-my_filter_states.py <mysql username> \
                                 <mysql password> \
                                 <database name> \
