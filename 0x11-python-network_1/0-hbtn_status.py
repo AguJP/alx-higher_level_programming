0ea676317dca9fa9234c3f074b62788387c53d99
AguJP <agujpa@gmail.com>
2023-09-02 10:31:36 -0700

    Whats my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,7 @@
 """ Script that fetches https://alx-intranet.hbtn.io/status """
 
 
-import urllib.request
-with urllib.request.urlopen(https://alx-intranet.hbtn.io/status)as response:
-    html = response.read()
+if __name__ == '__main__':
+    import urllib.request
+    with urllib.request.urlopen(https://alx-intranet.hbtn.io/status)as response:
+        html = response.read()
