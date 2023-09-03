e063a1806c843cf5788ddb4ef441c88d218c4c3c
AguJP <agujpa@gmail.com>
2023-09-02 18:28:09 -0700

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,12 +7,12 @@
 if __name__ == "__main__":
     import urllib.request
     import sys
-    import urllib.parse
     import urllib.error
 
     url = sys.argv[1]
 
-    with urllib.request.urlopen(url) as response:
-        print(response.read().decode('utf-8'))
-except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
