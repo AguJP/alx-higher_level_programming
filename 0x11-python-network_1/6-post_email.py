ccf1a4aa27f7d899f9fa34005057af0c70c1652a
AguJP <agujpa@gmail.com>
2023-09-02 19:23:48 -0700

    Post an email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email address
+- sends a POST request to the passed URL with the email as a parameter
+- displays the body of the response
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    email = {"email": sys.argv[2]}
+
+    req = requests.post(url, email)
+    print(req.text)
+
+    
