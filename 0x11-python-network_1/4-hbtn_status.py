c21f4db65c3c99b39695abb33651db9e4fb91ae6
AguJP <agujpa@gmail.com>
2023-09-02 18:52:21 -0700

    Whats my status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,8 +6,8 @@
 if __name__ == "__main__":
     import requests
 
-    request = requests.get('https://alx-intranet.hbtn.io/status')
-    disp = request.text
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    disp = req.text
 
     print("Body response:")
     print("\t- type: {}".format(type(disp)))
