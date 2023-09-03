5cbb5c4a5d1ec2a579ee9e0b6a5346588ac89ced
AguJP <agujpa@gmail.com>
2023-09-02 19:54:30 -0700

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""script that takes in a letter
+- Sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) == 1:
+        letter = ""
+    else:
+        letter = sys.argv[1]
+    q = {"q": letter}
+
+    try:
+        req = requests.post(url, q)
+        response = req.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
