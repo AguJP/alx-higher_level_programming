6b5e62d262091e2dc448cb6efcaf16129e206a48
AguJP <agujpa@gmail.com>
2023-09-02 20:50:03 -0700

    Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
     import requests
     import sys
 
-
     url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[
         2], sys.argv[1])
 
     req = requests.get(url)
     commits = req.json()
+
     try:
         for j in range(10):
             print("{}: {}".format(commits[j].get("sha"), commits[j].get(
